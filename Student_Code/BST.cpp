3be17e4209d31a6745af2945e446c66936bdbf34
Will Pinkston <william@pinkston.com>
2016-11-22 01:58:10 -0700

    files upload

---
+++ Student_Code/BST.cpp
@@ -0,0 +1,189 @@
+//
+//  BST.cpp
+//  6_lab_tester
+//
+//  Created by William Pinkston on 11/21/16.
+//  Copyright © 2016 Xunil_programming. All rights reserved.
+//
+
+#include <stdio.h>
+#include "BST.h"
+
+//----------------------------------------------------------
+
+BST::BST()
+{
+    _Root = NULL;
+}
+
+BST::~BST() {}
+
+//----------------------------------------------------------
+
+NodeInterface* BST::getRootNode()
+{
+    return _Root;
+}
+
+//----------------------------------------------------------
+
+bool BST::add(int data)
+{
+//    Node* addR = _Root;
+    return addHelper(_Root, data);
+}
+bool BST::remove(int data)
+{
+    Node* remR = _Root;
+    return removeHelper(remR, data);
+}
+
+//-----
+//-----
+
+bool BST::addHelper(Node* &here, int data)
+{
+    if (here == NULL)
+    {
+        here = new Node();
+        here->value = data;
+        return true;
+    }
+    if (here->value == data)
+    {
+        return false;
+    }
+    
+    if (data < here->value)
+    {
+        return addHelper(here->leftChild, data);
+    }
+    else if (data > here->value)
+    {
+        return addHelper(here->rightChild, data);
+    }
+    
+    else
+    {
+        return false;
+    }
+}
+
+//-----
+//-----
+
+//what if you try to remove the root?
+bool BST::removeHelper(Node* &here, int data)
+{
+    Node* parentNode = findVal(here, data);
+    if (parentNode == NULL) return false;
+    if (data == parentNode->value)
+    {
+        Node* replaceRoot = findLeftGreatest(parentNode->leftChild);
+        int hold = replaceRoot->value;
+        remove(hold);
+        parentNode->value = hold;
+    }
+    
+    Node* temp = NULL;
+    bool isLeft = false;
+    if (data < parentNode->value)
+    {
+        temp = parentNode->leftChild;
+        isLeft = true;
+    }
+    if (data > parentNode->value)
+    {
+        temp = parentNode->rightChild;
+        isLeft = false;
+    }
+    
+    
+    if (temp->getLeftChild() != NULL && temp->getRightChild() != NULL)
+    {
+        Node* replaceVal = findLeftGreatest(temp->leftChild);
+        int hold = replaceVal->value;
+        remove(hold);
+        temp->value = hold;
+    }
+    else
+    {
+        //use the skip link thing
+        if (temp->getLeftChild() == NULL)
+        {
+            if (isLeft)
+            {
+                parentNode->leftChild = temp->rightChild;
+            }
+            else
+            {
+                parentNode->rightChild = temp->rightChild;
+            }
+        }
+        else
+        {
+            if (isLeft)
+            {
+                parentNode->leftChild = temp->leftChild;
+            }
+            else
+            {
+                parentNode->rightChild = temp->leftChild;
+            }
+        }
+        delete temp;
+    }
+    return true;
+}
+
+//-----
+//-----
+
+BST::Node* BST::findVal(Node* find, int data)
+{
+    //check the root
+    if (find == NULL) return NULL;
+    if (find->value == data) return find;
+    
+    return findValR(find, data);
+}
+
+BST::Node* BST::findValR(Node* find, int data)
+{
+    //checks one node ahead, so that it can return the parent node
+    if (find->leftChild == NULL && find->rightChild == NULL) return NULL;
+    if (data < find->value && find->leftChild == NULL) return NULL;
+    if (data < find->value)
+    {
+        if (find->leftChild->value == data) return find;
+        return findValR(find->leftChild, data);
+    }
+    else
+    {
+        if (data > find->value && find->rightChild == NULL) return NULL;
+        if (find->rightChild->value == data) return find;
+        return findValR(find->rightChild, data);
+    }
+}
+
+//-----
+//-----
+
+//unchecked
+BST::Node* BST::findLeftGreatest(Node* remL_Child)
+{
+    if (remL_Child->rightChild == NULL)
+    {
+        return remL_Child;
+    }
+    else return findLeftGreatest(remL_Child->rightChild);
+}
+
+//----------------------------------------------------------
+
+void BST::clear()
+{
+    
+}
+
+//----------------------------------------------------------
---
+++ Student_Code/BST.h
@@ -0,0 +1,63 @@
+//
+//  BST.h
+//  6_lab_tester
+//
+//  Created by William Pinkston on 11/21/16.
+//  Copyright © 2016 Xunil_programming. All rights reserved.
+//
+
+#ifndef BST_h
+#define BST_h
+
+#include "BSTInterface.h"
+#include "NodeInterface.h"
+//#include "Node.h"
+
+class BST : public BSTInterface {
+private:
+    //
+    class Node : public NodeInterface {
+    public:
+        Node* leftChild;
+        Node* rightChild;
+        int value;
+
+        Node() {
+            leftChild = NULL;
+            rightChild = NULL;
+        }
+        ~Node() {}
+        int getData() {
+            return value;
+        }
+        NodeInterface *getLeftChild() {
+            return leftChild;
+        }
+        NodeInterface *getRightChild() {
+            return rightChild;
+        }
+    };
+    //
+    Node* _Root;
+public:
+    BST();
+    ~BST();
+    NodeInterface * getRootNode();
+    bool add(int data);
+    bool remove(int data);
+    void clear();
+    // helper functions
+    bool addHelper(Node* &here, int data);
+    bool removeHelper(Node* &here, int data);
+    Node* findVal(Node* find, int data);
+    Node* findValR(Node* find, int data);
+    Node* findLeftGreatest(Node* remL_Child);
+};
+
+#endif /* BST_h */
+
+
+
+
+
+
